17f9c90413f75b9f0def8cfa29f2623c46dbfde7
Hyeeun <hyeeuni.mok@gmail.com>
2019-04-10 23:23:37 -0400

    adding movie db API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
+import MovieRow from './MovieRow.js';
+import $ from 'jquery';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    console.log("This is my initializer")
+    this.state = {}
+    // console.log("This is my initializer")
 
-    const movies = [
-      {id:0, poster_src:"https://image.tmdb.org/t/p/w370_and_h556_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Avengers: Infinity War", overview: "This is my first overview"},
-      {id:1, poster_src:"https://image.tmdb.org/t/p/w370_and_h556_bestv2/cezWGskPY5x7GaglTTRN4Fugfb8.jpg", title: "The Avengers: Infinity War", overview: "This is my second overview"}
-    ]
+    // const movies = [
+    //   {id:0, poster_src:"https://image.tmdb.org/t/p/w370_and_h556_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg", title: "Avengers: Infinity War", overview: "This is my first overviewddddddddddddddddddddddddddddddddddddddddd"},
+    //   {id:1, poster_src:"https://image.tmdb.org/t/p/w370_and_h556_bestv2/cezWGskPY5x7GaglTTRN4Fugfb8.jpg", title: "The Avengers: Infinity War", overview: "This is my second overview eeeeeeeeeeeeeffffffffffffffffe"}
+    // ]
 
-    var movieRows = []
-    movies.forEach((movie) => {
-      console.log(movie.title)
-      const movieRow = <table key={movie.id}>
-        <tbody>
-          <tr>
-            <td>
-              <img alt="poster" src={movie.poster_src}/>
-            </td>
-            <td>
-              {movie.title}
-            </td>
-          </tr>
-          </tbody>
-      </table>
-      movieRows.push(movieRow)
+    // var movieRows = []
+    // movies.forEach((movie) => {
+    //   console.log(movie.title)
+    //   const movieRow = <MovieRow movie={movie} />
+    //   movieRows.push(movieRow)
+    // })
+
+    // this.state = {rows: movieRows}
+    this.performSerch()
+  }
+
+  performSerch() {
+    console.log("Perform search using moviedb")
+    const urlString ="https://api.themoviedb.org/3/search/movie?api_key=1f1b95c89a3439518717be62697c7775&query=avengers"
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        // console.log(searchResults)
+        const results = searchResults.results
+        // console.log(results[0])
+
+        results.forEach((movie) => {
+          console.log(movie.title)
+        })
+      },
+      error: (xhr, status, err) =>{
+        console.error("Failed to fetch data")
+      }
     })
-
-    this.state = {rows: movieRows}
   }
 
   render() {
     return (
-      <div className="App">
+      <div>
         <table className="titleBar">
           <tbody>
             <tr>
